dae4225a7206bdfd6d54142e11344e2e265304e1
ivatwist <39283123+ivatwist@users.noreply.github.com>
2022-06-13 15:16:51 -0600

    Subir footer.js

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,12 @@
+function Footer(){
+    return(
+
+
+        <>
+        <i className="textoFooter">
+            Creado por Roy, Luis e Ivania
+        </i>
+        </>
+    );
+}
+export default Footer;

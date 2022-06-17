cdbf15f3d8b9a77219bffd798f8d9d96cf3470fb
ivatwist <39283123+ivatwist@users.noreply.github.com>
2022-06-17 10:45:59 -0600

    Cambio para que funcione redirecciones URL

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './Pages/Home';
+import App from './Pages/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 

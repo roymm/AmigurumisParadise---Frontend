407f5df008a3903810d7e9d23b5d379750e1c0c6
roymm <royamunoz@protonmail.com>
2022-07-01 03:34:23 -0600

    Adds a car for the products on home

---
+++ src/Components/ProductCard/index.js
@@ -0,0 +1,26 @@
+const ProductCard = ({productData}) => {
+    return (
+        <>
+        <div
+            className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden shadow-xl group-hover:opacity-75 lg:h-80 lg:aspect-none">
+            <img src={productData.picture}
+                 className="w-full h-full object-center object-cover lg:w-full lg:h-full" alt="">
+
+            </img>
+        </div>
+        <div className="mt-4 flex justify-between">
+            <div>
+                <h3 className="text-lg text-gray-700">
+                    <a href="#">
+                        <span aria-hidden="true" className="absolute inset-0"></span>
+                        {productData.name}
+                    </a>
+                </h3>
+            </div>
+            <p className="text-lg font-medium text-gray-900">${productData.price}</p>
+        </div>
+</>
+)
+}
+
+export default ProductCard;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,8 +3,10 @@ import Logo from "../../Components/Logo";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import Carousel from "../../Components/Carousel";
-import {getAllProducts} from "../../Slices/productSlice";
-import {useDispatch, useSelector} from "react-redux";
+import ProductCard from "../../Components/ProductCard";
+//import {getAllProducts} from "../../Slices/productSlice";
+
+//import {useDispatch, useSelector} from "react-redux";
 
 function Home() {
     /*PARA PRUEBA CON DISPATCH Y SLICES
@@ -45,38 +47,16 @@ function Home() {
                 <Carousel/>
 
             </div>
-            <button onClick={getAllProducts}>Imprimir productos</button>
-            <div className="grid grid-cols-3 pl-40 pt-20">
-                <link rel="stylesheet"
-                      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
-                {products.map((slide, index) => {
-
+            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                {products.map((product) => {
                     return (
-
-                        <div className="pt-10">
-
-                            <img src={slide.picture} alt="imagen" className='w-40 h-32 rounded-3xl'></img>
-                            <div className="text-rose-500 pl-10 ml-5">{slide.price}</div>
-                            <span class="material-symbols-outlined pl-5">grade</span>
-                            <span class="material-symbols-outlined">grade</span>
-                            <span class="material-symbols-outlined">grade</span>
-                            <span class="material-symbols-outlined">grade</span>
-                            <span class="material-symbols-outlined">grade</span>
-                            <div>
-                                <button
-                                    className="bg-rose-500 ml-4 py-2 px-2 text-white rounded-md cursor-pointer">Agregar
-                                    a carrito
-                                </button>
-                            </div>
-
-
+                        <div className="group relative">
+                            <ProductCard productData={product}></ProductCard>
                         </div>
-
                     )
-
-
                 })}
-
+                </div>
             </div>
 
             <div className="text-rose-500 pt-60 text-center">
